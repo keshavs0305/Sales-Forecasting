58a9ab4d346e495a15479afa9f0de3a5e1596fd2
Keshav <keshavsarda503@gmail.com>
2021-10-22 19:16:24 +0530

    model1 done

--- model.py
+++ model.py
@@ -17,16 +17,6 @@ def preds(cusid, f):
     data, n = data_cus(df, df2, cusid, f)
     data = scale.fit_transform(data.head(len(data) - n))
 
-    # train, test = data[:round(len(data)*0.8)], data[round(len(data)*0.8):]\n",
-    # x_train, x_test, y_train, y_test = train[:,:-1], test[:,:-1], train[:,-1], test[:,-1]\n",
-
-    # pred = [np.append(x_test[i,:],model.predict(x_test[i,:].reshape(1,-1))) for i in range(len(x_test))]\n",
-    # true = [np.append(x_test[i,:],y_test[i].reshape(1,-1)) for i in range(len(x_test))]\n",
-    # plt.plot(scale.inverse_transform(pred)[:,-1],label='preds')\n",
-    # plt.plot(scale.inverse_transform(true)[:,-1])\n",
-    # plt.legend()\n",
-    # plt.show()\n",
-
     model = xgb.XGBRegressor().fit(data[:, :-1], data[:, -1])
 
     x = scale.transform(np.array(new_data(cusid, f)).reshape(1, -1))
